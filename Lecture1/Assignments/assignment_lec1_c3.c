35703b79a66af26d6dac6b817724e8056ee1867c
front-git <100923963+front-git@users.noreply.github.com>
2022-03-04 17:41:37 +0800

    Add files via upload

---
+++ Lecture1/Assignments/assignment_lec1_c3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  int sum;
+  sum = 87 - 15;
+  printf("This program is designed to subtract 15 from 87.\n");
+  printf("The difference is %d.\n", sum);
+  return 0;
+}
